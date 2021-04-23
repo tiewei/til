01890bf19dddb66f58fc22a34ae2cebfdc6b85e2
Antoine Cotten <hello@acotten.com>
2021-04-23 18:32:15 +0200

    Add helper for generating SecretKeySelectors

--- internal/sdk/secrets/secrets.go
+++ internal/sdk/secrets/secrets.go
@@ -4,63 +4,41 @@ package secrets
 // be passed to unstructured.SetNestedMap.
 type secretKeySelector map[string]interface{}
 
+// newSecretKeySelector returns a secretKeySelector with the given secret name
+// and data key.
+func newSecretKeySelector(name, key string) secretKeySelector {
+	return secretKeySelector{
+		"name": name,
+		"key":  key,
+	}
+}
+
 // SecretKeyRefsAWS returns secret key selectors for the "aws" secret class.
 func SecretKeyRefsAWS(secretName string) (accessKeyID, secretAccessKey secretKeySelector) {
-	return secretKeySelector{
-			"name": secretName,
-			"key":  secrClassAWSAccessKeyID,
-		},
-		secretKeySelector{
-			"name": secretName,
-			"key":  secrClassAWSSecretAccessKey,
-		}
+	return newSecretKeySelector(secretName, secrClassAWSAccessKeyID),
+		newSecretKeySelector(secretName, secrClassAWSSecretAccessKey)
 }
 
 // SecretKeyRefsConfluent returns secret key selectors for the "confluent" secret class.
 func SecretKeyRefsConfluent(secretName string) (passwd secretKeySelector) {
-	return secretKeySelector{
-		"name": secretName,
-		"key":  secrClassConfluentPasswd,
-	}
+	return newSecretKeySelector(secretName, secrClassConfluentPasswd)
 }
 
 // SecretKeyRefsKafkaSASL returns secret key selectors for the "kafka_sasl" secret class.
 func SecretKeyRefsKafkaSASL(secretName string) (usr, passwd, typ secretKeySelector) {
-	return secretKeySelector{
-			"name": secretName,
-			"key":  secrClassKafkaSASLUser,
-		},
-		secretKeySelector{
-			"name": secretName,
-			"key":  secrClassKafkaSASLPasswd,
-		},
-		secretKeySelector{
-			"name": secretName,
-			"key":  secrClassKafkaSASLType,
-		}
-
+	return newSecretKeySelector(secretName, secrClassKafkaSASLUser),
+		newSecretKeySelector(secretName, secrClassKafkaSASLPasswd),
+		newSecretKeySelector(secretName, secrClassKafkaSASLType)
 }
 
 // SecretKeyRefsTLS returns secret key selectors for the "tls" secret class.
 func SecretKeyRefsTLS(secretName string) (cert, key, caCert secretKeySelector) {
-	return secretKeySelector{
-			"name": secretName,
-			"key":  secrClassTLSCert,
-		},
-		secretKeySelector{
-			"name": secretName,
-			"key":  secrClassTLSKey,
-		},
-		secretKeySelector{
-			"name": secretName,
-			"key":  secrClassTLSCACert,
-		}
+	return newSecretKeySelector(secretName, secrClassTLSCert),
+		newSecretKeySelector(secretName, secrClassTLSKey),
+		newSecretKeySelector(secretName, secrClassTLSCACert)
 }
 
 // SecretKeyRefsZendesk returns secret key selectors for the "zendesk" secret class.
 func SecretKeyRefsZendesk(secretName string) (token secretKeySelector) {
-	return secretKeySelector{
-		"name": secretName,
-		"key":  secrClassZendeskToken,
-	}
+	return newSecretKeySelector(secretName, secrClassZendeskToken)
 }
--- translation/doc.go
+++ translation/doc.go
@@ -1,4 +1,4 @@
 // Package translation contains interfaces that allow components expressed in
-// Bridge Definition Files to be translated into concrete resource definitions,
-// such as Kubernetes objects.
+// Bridge Description Files to be translated into concrete resource
+// definitions, such as Kubernetes objects.
 package translation
