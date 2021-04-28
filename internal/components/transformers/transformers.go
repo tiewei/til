b890c086cd4e59626aa5d36815ecfd350bc854dc
tzununbekov <t.zununbekov@gmail.com>
2021-04-28 15:44:28 +0600

    Function transformer added

--- internal/components/transformers/transformers.go
+++ internal/components/transformers/transformers.go
@@ -3,4 +3,5 @@ package transformers
 // All includes all "transformer" component types supported by TriggerMesh.
 var All = map[string]interface{}{
 	"bumblebee": (*Bumblebee)(nil),
+	"function":  (*Function)(nil),
 }
