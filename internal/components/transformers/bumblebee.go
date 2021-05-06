b6e1993796b6116b647fe48c4eb7160b12d3a9e7
Antoine Cotten <hello@acotten.com>
2021-05-06 18:45:09 +0200

    Clean up implementation of "bumblebee" transformer
    * Document structure of decoded spec.
    * Rename vars to reflect cty.Value vs. Go primitive type.

--- internal/components/transformers/bumblebee.go
+++ internal/components/transformers/bumblebee.go
@@ -3,15 +3,13 @@ package transformers
 import (
 	"github.com/hashicorp/hcl/v2/hcldec"
 	"github.com/zclconf/go-cty/cty"
+
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"bridgedl/internal/sdk/k8s"
 	"bridgedl/translation"
 )
 
-// TODO(antoineco): this is a mock implementation. It exists only to allow
-// testing the generator manually using the config.brg.hcl file at the root of
-// the repo while this code is still at the stage of prototype.
 type Bumblebee struct{}
 
 var (
@@ -56,22 +54,56 @@ func (*Bumblebee) Spec() hcldec.Spec {
 			Nested:   nestedSpec,
 		},
 	}
+
+	/*
+		Example of value decoded from the spec above, for a "context"
+		and a "data" block, both containing two nested "operation" blocks
+		with one or more "path" sub-blocks:
+
+		v: (map[string]interface {}) (len=2) {
+		 "context": ([]interface {}) (len=2) {
+		  (map[string]interface {}) (len=2) {
+		   "operation": (string) "store",
+		   "path": ([]interface {}) (len=2) {
+		    (map[string]interface {}) (len=2) {
+		     "key": (string) "$id",
+		     "value": (string) "id"
+		    },
+		    (map[string]interface {}) (len=2) {
+		     "key": (string) "$type",
+		     "value": (string) "type"
+		    }
+		   }
+		  },
+		  (map[string]interface {}) (len=2) {
+		   "operation": (string) "add",
+		   "path": ([]interface {}) (len=1) {
+		    (map[string]interface {}) (len=2) {
+		     "key": (string) "idtype",
+		     "value": (string) "${id}-${type}"
+		    }
+		   }
+		  }
+		 },
+		 "data": ([]interface {}) (len=2) { ... }
+		}
+	*/
 }
 
 // Manifests implements translation.Translatable.
 func (*Bumblebee) Manifests(id string, config, eventDst cty.Value) []interface{} {
 	var manifests []interface{}
 
-	s := &unstructured.Unstructured{}
-	s.SetAPIVersion("flow.triggermesh.io/v1alpha1")
-	s.SetKind("Transformation")
-	s.SetName(k8s.RFC1123Name(id))
+	transf := &unstructured.Unstructured{}
+	transf.SetAPIVersion("flow.triggermesh.io/v1alpha1")
+	transf.SetKind("Transformation")
+	transf.SetName(k8s.RFC1123Name(id))
 
 	context := parseBumblebeeOperations(config.GetAttr("context").AsValueSlice())
-	_ = unstructured.SetNestedSlice(s.Object, context, "spec", "context")
+	_ = unstructured.SetNestedSlice(transf.Object, context, "spec", "context")
 
 	data := parseBumblebeeOperations(config.GetAttr("data").AsValueSlice())
-	_ = unstructured.SetNestedSlice(s.Object, data, "spec", "data")
+	_ = unstructured.SetNestedSlice(transf.Object, data, "spec", "data")
 
 	sinkRef := eventDst.GetAttr("ref")
 	sink := map[string]interface{}{
@@ -79,34 +111,46 @@ func (*Bumblebee) Manifests(id string, config, eventDst cty.Value) []interface{}
 		"kind":       sinkRef.GetAttr("kind").AsString(),
 		"name":       sinkRef.GetAttr("name").AsString(),
 	}
-	_ = unstructured.SetNestedMap(s.Object, sink, "spec", "sink", "ref")
+	_ = unstructured.SetNestedMap(transf.Object, sink, "spec", "sink", "ref")
 
-	return append(manifests, s)
-}
-
-func parseBumblebeeOperations(values []cty.Value) []interface{} {
-	var operations []interface{}
-	for _, operation := range values {
-		var paths []interface{}
-		for _, path := range operation.AsValueMap()["path"].AsValueSlice() {
-			p := make(map[string]interface{})
-			if key := path.AsValueMap()["key"]; !key.IsNull() {
-				p["key"] = key.AsString()
-			}
-			if value := path.AsValueMap()["value"]; !value.IsNull() {
-				p["value"] = value.AsString()
-			}
-			paths = append(paths, p)
-		}
-		operations = append(operations, map[string]interface{}{
-			"operation": operation.AsValueMap()["operation"].AsString(),
-			"paths":     paths,
-		})
-	}
-	return operations
+	return append(manifests, transf)
 }
 
 // Address implements translation.Addressable.
 func (*Bumblebee) Address(id string, _, _ cty.Value) cty.Value {
 	return k8s.NewDestination("flow.triggermesh.io/v1alpha1", "Transformation", k8s.RFC1123Name(id))
+}
+
+func parseBumblebeeOperations(operationVals []cty.Value) []interface{} {
+	operations := make([]interface{}, 0, len(operationVals))
+
+	for _, operationVal := range operationVals {
+		operationValMap := operationVal.AsValueMap()
+
+		operation := operationValMap["operation"].AsString()
+
+		pathVals := operationValMap["path"].AsValueSlice()
+		paths := make([]interface{}, 0, len(pathVals))
+		for _, pathVal := range pathVals {
+			path := make(map[string]interface{})
+
+			pathValMap := pathVal.AsValueMap()
+
+			if key := pathValMap["key"]; !key.IsNull() {
+				path["key"] = key.AsString()
+			}
+			if value := pathValMap["value"]; !value.IsNull() {
+				path["value"] = value.AsString()
+			}
+
+			paths = append(paths, path)
+		}
+
+		operations = append(operations, map[string]interface{}{
+			"operation": operation,
+			"paths":     paths,
+		})
+	}
+
+	return operations
 }
