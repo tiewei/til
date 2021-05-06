781486744f426c1a5fd865edaa26bbe07e0e81a8
Antoine Cotten <hello@acotten.com>
2021-05-06 19:42:51 +0200

    sdk: Add helper type for creating Unstructured objs

--- internal/sdk/k8s/k8s.go
+++ internal/sdk/k8s/k8s.go
@@ -1,6 +1,10 @@
 package k8s
 
-import "strings"
+import (
+	"strings"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
 
 // RFC1123Name sanitizes the given input string, ensuring it is a valid DNS
 // subdomain name (as defined in RFC 1123) which can be used as a Kubernetes
@@ -11,4 +15,74 @@ import "strings"
 // limited set of characters (see hclsyntax.ValidIdentifier).
 func RFC1123Name(id string) string {
 	return strings.ToLower(strings.ReplaceAll(id, "_", "-"))
+}
+
+// Object wraps an instance of unstructured.Unstructured to expose convenience
+// field setters.
+type Object struct {
+	u *unstructured.Unstructured
+}
+
+// NewObject returns an Object initialized with the given group/version, kind
+// and identifier.
+func NewObject(apiVersion, kind, id string) *Object {
+	o := &Object{
+		u: &unstructured.Unstructured{},
+	}
+
+	o.u.SetAPIVersion(apiVersion)
+	o.u.SetKind(kind)
+	o.u.SetName(RFC1123Name(id))
+
+	return o
+}
+
+// Unstructured returns the underlying unstructured.Unstructured.
+func (o *Object) Unstructured() *unstructured.Unstructured {
+	return o.u
+}
+
+// SetNestedField invokes unstructured.SetNestedField on the underlying
+// object's data.
+func (o *Object) SetNestedField(value interface{}, fields ...string) {
+	err := unstructured.SetNestedField(o.u.Object, value, fields...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SetNestedSlice invokes unstructured.SetNestedSlice on the underlying
+// object's data.
+func (o *Object) SetNestedSlice(value []interface{}, fields ...string) {
+	err := unstructured.SetNestedSlice(o.u.Object, value, fields...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SetNestedMap invokes unstructured.SetNestedMap on the underlying
+// object's data.
+func (o *Object) SetNestedMap(value map[string]interface{}, fields ...string) {
+	err := unstructured.SetNestedMap(o.u.Object, value, fields...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SetNestedStringSlice invokes unstructured.SetNestedStringSlice on the underlying
+// object's data.
+func (o *Object) SetNestedStringSlice(value []string, fields ...string) {
+	err := unstructured.SetNestedStringSlice(o.u.Object, value, fields...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SetNestedStringMap invokes unstructured.SetNestedStringMap on the underlying
+// object's data.
+func (o *Object) SetNestedStringMap(value map[string]string, fields ...string) {
+	err := unstructured.SetNestedStringMap(o.u.Object, value, fields...)
+	if err != nil {
+		panic(err)
+	}
 }
