c151262c7de1c48f6a6caf9d0d61385163c81ec4
Antoine Cotten <hello@acotten.com>
2021-05-12 21:02:25 +0200

    Add "aws_sns" source

---
+++ internal/components/sources/aws_sns.go
@@ -0,0 +1,53 @@
+package sources
+
+import (
+	"github.com/hashicorp/hcl/v2/hcldec"
+	"github.com/zclconf/go-cty/cty"
+
+	"bridgedl/internal/sdk/k8s"
+	"bridgedl/internal/sdk/secrets"
+	"bridgedl/translation"
+)
+
+type AWSSNS struct{}
+
+var (
+	_ translation.Decodable    = (*AWSSNS)(nil)
+	_ translation.Translatable = (*AWSSNS)(nil)
+)
+
+// Spec implements translation.Decodable.
+func (*AWSSNS) Spec() hcldec.Spec {
+	return &hcldec.ObjectSpec{
+		"arn": &hcldec.AttrSpec{
+			Name:     "arn",
+			Type:     cty.String,
+			Required: true,
+		},
+		"credentials": &hcldec.AttrSpec{
+			Name:     "credentials",
+			Type:     k8s.ObjectReferenceCty,
+			Required: true,
+		},
+	}
+}
+
+// Manifests implements translation.Translatable.
+func (*AWSSNS) Manifests(id string, config, eventDst cty.Value) []interface{} {
+	var manifests []interface{}
+
+	s := k8s.NewObject("sources.triggermesh.io/v1alpha1", "AWSSNSSource", k8s.RFC1123Name(id))
+
+	arn := config.GetAttr("arn").AsString()
+	s.SetNestedField(arn, "spec", "arn")
+
+	credsSecretName := config.GetAttr("credentials").GetAttr("name").AsString()
+	accKeySecretRef, secrKeySecretRef := secrets.SecretKeyRefsAWS(credsSecretName)
+	s.SetNestedMap(accKeySecretRef, "spec", "credentials", "accessKeyID", "valueFromSecret")
+	s.SetNestedMap(secrKeySecretRef, "spec", "credentials", "secretAccessKey", "valueFromSecret")
+
+	sink := k8s.DecodeDestination(eventDst)
+	s.SetNestedMap(sink, "spec", "sink", "ref")
+
+	return append(manifests, s.Unstructured())
+}
--- internal/components/sources/sources.go
+++ internal/components/sources/sources.go
@@ -8,6 +8,7 @@ var All = map[string]interface{}{
 	"aws_dynamodb":         (*AWSDynamoDB)(nil),
 	"aws_kinesis":          (*AWSKinesis)(nil),
 	"aws_s3":               (*AWSS3)(nil),
+	"aws_sns":              (*AWSSNS)(nil),
 	"aws_sqs":              (*AWSSQS)(nil),
 	"azure_activity_logs":  (*AzureActivityLogs)(nil),
 	"azure_blob_storage":   (*AzureBlobStorage)(nil),
